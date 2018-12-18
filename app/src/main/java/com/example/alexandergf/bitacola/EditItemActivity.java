f714177cc4b5b98d9de0575b04b730d99bcad36c
alexandergf <alexgferreiro@hotmail.com>
2018-12-18 09:46:15 +0100

    v 0.4.1 -> sigo creando el item Ja esta fet el layout principal de EditItemActivity

--- app/src/main/java/com/example/alexandergf/bitacola/EditItemActivity.java
+++ app/src/main/java/com/example/alexandergf/bitacola/EditItemActivity.java
@@ -5,18 +5,48 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditItemActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
 
+        final Button btn_cancel=findViewById(R.id.cancelButton);
+        final Button btn_save=findViewById(R.id.saveButton);
+        final ImageButton addImageButton=findViewById(R.id.addImageButton);
+        final ImageButton selectImageButton=findViewById(R.id.selectImageButton);
+
+
+        final EditText editTitle=findViewById(R.id.editTitle);
+        final EditText editDate=findViewById(R.id.editDate);
+        final EditText editDesc=findViewById(R.id.editDesc);
+
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
